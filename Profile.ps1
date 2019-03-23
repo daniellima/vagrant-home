07231d6ee82312851367e29392b7c0d34085aef3
Daniel Lima dos Santos <daniellima.pessoal@gmail.com>
2019-03-23 16:31:22 +0000

    Add powershell Profile to more easily operate this vm
    The 'home' function for example can be used to swith from powershell
    to the vm if inside one of the shared folders inside 'code'

---
+++ Profile.ps1
@@ -0,0 +1,15 @@
+# See where to paste this file:
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-6
+# In latest windows versions, it's probably "$HOME\Documents\WindowsPowerShell"
+
+Function home {
+    ssh home -t "cd /code/$(Split-Path -Path . -Leaf); bash -li"
+}
+Function homeup {
+    cd $HOME/code/vagrant-home
+    vagrant up
+}
+Function homep {
+    cd $HOME/code/vagrant-home
+    vagrant up --provision
+}
