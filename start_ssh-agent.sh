fae0a80ec16688b8551836c5a5936e8640cf7f9c
vagrant <daniellima.pessoal@gmail.com>
2019-06-11 09:41:22 -0300

    Make 8h the default for all ssh keys
    Since keys are added automatically by AddKeysToAgent, the maximum
    lifetime of a key must be set globally

--- start_ssh-agent.sh
+++ start_ssh-agent.sh
@@ -2,11 +2,11 @@ SSH_ENV="$HOME/.ssh/environment"
 
 function start_agent {
     echo "Initialising new SSH agent..."
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+    /usr/bin/ssh-agent -t 8h | sed 's/^echo/#echo/' > "${SSH_ENV}"
     echo succeeded
     chmod 600 "${SSH_ENV}"
     . "${SSH_ENV}" > /dev/null
-    /usr/bin/ssh-add -t 8h;
+    /usr/bin/ssh-add;
 }
 
 # Source SSH settings, if applicable
